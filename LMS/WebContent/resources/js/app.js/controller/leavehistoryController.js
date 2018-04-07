3bd31ddc2f48fd9e7549f8e31900c89f69274e4d
MD. REAZUL HAQUE <reazulhaque20@gmail.com>
2018-04-07 11:29:18 +0600

    Project Rectifications

--- LMS/WebContent/resources/js/app.js/controller/leavehistoryController.js
+++ LMS/WebContent/resources/js/app.js/controller/leavehistoryController.js
@@ -12,13 +12,9 @@ App
 
 			function($scope, $http, leavehistoryService,
 				$timeout, $filter,NgTableParams,$location) {
-<<<<<<< HEAD
 				
 				$scope.testMsg = "Test Message New";
-=======
-				var self = this;
-				self.testMsg = "Test Message New";
->>>>>>> e3eae446a69a66540732f6ec34029e0cf68f87af
+
 				/*$scope.leavehistory = function(user_id){
 					$scope.user_id;
 					leavehistoryService.leavehistory($scope.user_id).then(
@@ -36,23 +32,14 @@ App
 							});
 				};*/
 				
-<<<<<<< HEAD
-				var self = this;
-				self.leavehistory = function(user_id){
+				$scope.leavehistory = function(user_id){
 					$scope.user_id;
 					leavehistoryService.leavehistory($scope.user_id).then(
 							function(d) {
 								$scope.testMsg = d.message;
-=======
-				
-				self.leavehistory = function(user_id){
-					leavehistoryService.leavehistory(user_id).then(
-							function(d) {
-								self.testMsg1 = "Test";
->>>>>>> e3eae446a69a66540732f6ec34029e0cf68f87af
 								console.log("Success.",d.message);
 								var data = d.listLmsLeaveBalance;
-								self.tableParams = new NgTableParams({}, { dataset: data});
+								$scope.tableParams = new NgTableParams({}, { dataset: data});
 								
 							},
 							function(errResponse) {
@@ -62,13 +49,4 @@ App
 							});
 				};
 				
-<<<<<<< HEAD
-				self.leavehistory(2);
-				
-				//$scope.leavehistory(2);
-=======
-				
-				
-				//self.leavehistory(2);
->>>>>>> e3eae446a69a66540732f6ec34029e0cf68f87af
 			} ]);
--- LMS/WebContent/resources/js/app.js/controller/testController.js
+++ LMS/WebContent/resources/js/app.js/controller/testController.js
@@ -42,5 +42,6 @@ App
 					$scope.processDropzone();
 					$scope.resetDropzone();
 				};
+								
 				
 			} ]);
