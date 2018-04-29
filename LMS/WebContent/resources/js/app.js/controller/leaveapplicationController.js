72e0471832f08175c92e9a4a07a3e44073ec3a83
MD. REAZUL HAQUE <reazulhaque20@gmail.com>
2018-04-29 15:29:31 +0600

    Update
    Update

--- LMS/WebContent/resources/js/app.js/controller/leaveapplicationController.js
+++ LMS/WebContent/resources/js/app.js/controller/leaveapplicationController.js
@@ -188,7 +188,7 @@ App
 					//$scope.leaveapplication.leaveAvailable = $scope.leaveavailable;
 					$scope.leaveapplication.leaveTaken = $scope.leaveTaken;
 					$scope.leaveapplication.lmsLeaveType = $scope.leavetype;
-					$scope.leaveapplication.lmsUserByUserId=$scope.ddReliever
+					$scope.leaveapplication.userId= parseInt($scope.userid) ;
 					//$scope.leaveapplication.lmsLeaveType.type = $scope.appStatus.type;
 					$scope.leaveapplication.leaveBalance = $scope.leaveBalance;
 					$scope.leaveapplication.lmsUserByReliverEmailAddressUserId = $scope.ddReliever;
